0cf0e94116cba85255ec8e3887d8c63c6e89554d
Erickbci <batista.erick07@hotmail.com>
2023-05-19 20:57:54 -0300

    Cadastrando imóveis

---
+++ cadastro.js
@@ -0,0 +1,16 @@
+const botao = document.getElementById('cadastrar')
+  
+botao.addEventListener('click', () => {
+    const imoveis = [];
+
+    const imovel = {};
+    imovel.proprietario = document.getElementById('nomeDoProprietario').value;
+    imovel.quartos = document.getElementById('quantidadeDeQuartos').value;
+    imovel.banheiros = document.getElementById('quantidadeDeBanheiros').value;
+    imovel.garagem = document.getElementById('possuiGaragem').value;
+
+    imoveis.push(imovel)
+    alert('Imóvel Cadastrado com sucesso');
+    console.log(imoveis);
+
+})
